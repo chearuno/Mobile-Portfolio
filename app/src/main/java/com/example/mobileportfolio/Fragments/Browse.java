38624f96a658be23dd2b596646fd37aa7c33eb48
Chethana Arunodh <chethana0777@gmail.com>
2019-03-04 13:49:04 +0530

    Create Browse fragment top buttons

--- app/src/main/java/com/example/mobileportfolio/Fragments/Browse.java
+++ app/src/main/java/com/example/mobileportfolio/Fragments/Browse.java
@@ -15,7 +15,7 @@ public class Browse extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view, container, false);
+        return inflater.inflate(R.layout.fragment_browse, container, false);
     }
 
 }
